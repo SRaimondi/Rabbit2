996d14508a1d5b39e3cc1f009b70ac9c06bd9c16
Simone Raimondi <simo@cleverit.ch>
2019-03-28 14:17:42 +0100

    Added BVHBuildNode struct

--- source/bvh/bvh.cpp
+++ source/bvh/bvh.cpp
@@ -9,6 +9,29 @@
 namespace Geometry
 {
 
+// BVH building node
+struct BVHBuildNode
+{
+    // Create leaf node
+    BVHBuildNode(unsigned int first_triangle, unsigned int num_triangles, const BBox& bounds) noexcept
+        : first_triangle_index{ first_triangle }, num_triangles{ num_triangles }, bounds{ bounds }
+    {}
+
+    // Create internal node
+    BVHBuildNode(unsigned int split_axis, std::unique_ptr<BVHBuildNode> c0, std::unique_ptr<BVHBuildNode> c1) noexcept
+        : children{ std::move(c0), std::move(c1) }, split_axis{ split_axis }
+    {}
+
+    // Index of the first triangle and number of triangles in it
+    unsigned int first_triangle_index, num_triangles;
+    // Bounds of the node
+    BBox bounds;
+    // Axis used for the split
+    unsigned int split_axis;
+    // Children nodes
+    std::array<std::unique_ptr<BVHBuildNode>, 2> children;
+};
+
 BVH::BVH(const BVHConfig& config, const std::vector<Triangle>& tr)
     : configuration{ config }, triangles{ tr }
 {
@@ -19,13 +42,15 @@ BVH::BVH(const BVHConfig& config, const std::vector<Triangle>& tr)
         return;
     }
 
-    // Create list with the information for each triangle
+    // Create list with information for each triangle
     std::vector<TriangleInfo> triangle_info;
     triangle_info.reserve(triangles.size());
     for (unsigned int i = 0; i != triangles.size(); i++)
     {
         triangle_info.emplace_back(i, triangles[i].Bounds());
     }
+
+
 }
 
 } // Geometry namespace
