f0a0576defea27ec1f88f0d2cd95edfcac926c48
Simone Raimondi <simo@cleverit.ch>
2019-04-09 15:59:47 +0200

    Working on image integrator

--- source/integrator/image_integrator.cpp
+++ source/integrator/image_integrator.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "image_integrator.hpp"
+
+namespace Rabbit
+{
+
+ImageIntegrator::ImageIntegrator(const Geometry::Point2ui& tile_size, unsigned int spp) noexcept
+    : tile_size{ tile_size }, samples_per_pixel{ spp }
+{}
+
+void ImageIntegrator::RenderImage(const Scene& scene, const Camera& camera, Film& film) const noexcept
+{
+
+}
+
+std::vector<Tile> ImageIntegrator::GenerateTiles(unsigned int image_width, unsigned int image_height) const noexcept
+{
+    // Compute number of tiles
+    const unsigned int num_tiles_width{ DivideUp(image_width, tile_size.x) };
+    const unsigned int num_tiles_height{ DivideUp(image_height, tile_size.y) };
+
+    // Generate tiles
+    std::vector<Tile> tiles;
+    for (unsigned int tile_h = 0; tile_h != num_tiles_height; tile_h++)
+    {
+        for (unsigned int tile_w = 0; tile_w != num_tiles_width; tile_w++)
+        {
+            const Geometry::Point2ui start{ tile_w * tile_size.x, tile_h * tile_size.y };
+            const Geometry::Point2ui end{ std::min(start.x + tile_size.x, image_width),
+                                          std::min(start.y + tile_size.y, image_height) };
+            tiles.emplace_back(start, end);
+        }
+    }
+
+    return tiles;
+}
+
+} // Rabbit namespace
+
--- source/integrator/image_integrator.hpp
+++ source/integrator/image_integrator.hpp
@@ -5,4 +5,40 @@
 #ifndef RABBIT2_IMAGE_INTEGRATOR_HPP
 #define RABBIT2_IMAGE_INTEGRATOR_HPP
 
+#include "scene/scene.hpp"
+#include "camera/camera.hpp"
+#include "film/film.hpp"
+
+namespace Rabbit
+{
+
+// Tile in the image to render for a thread
+struct Tile
+{
+    constexpr Tile(const Geometry::Point2ui& start, const Geometry::Point2ui& end) noexcept
+        : tile_start{ start }, tile_end{ end }
+    {}
+
+    const Geometry::Point2ui tile_start;
+    const Geometry::Point2ui tile_end;
+};
+
+class ImageIntegrator
+{
+public:
+    ImageIntegrator(const Geometry::Point2ui& tile_size, unsigned int spp) noexcept;
+
+    void RenderImage(const Scene& scene, const Camera& camera, Film& film) const noexcept;
+
+private:
+    std::vector<Tile> GenerateTiles(unsigned int image_width, unsigned int image_height) const noexcept;
+
+    // Size of tiles to render
+    const Geometry::Point2ui tile_size;
+    // Number of samples for each pixel
+    const unsigned int samples_per_pixel;
+};
+
+} // Rabbit namespace
+
 #endif //RABBIT2_IMAGE_INTEGRATOR_HPP
