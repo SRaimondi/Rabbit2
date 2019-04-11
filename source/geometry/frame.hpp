4e77c153f20356b4d219250ad711dc1c7ebc7ac0
Simone Raimondi <simo@cleverit.ch>
2019-04-11 10:41:04 +0200

    Added Frame struct for local coordinates systems

---
+++ source/geometry/frame.hpp
@@ -0,0 +1,56 @@
+//
+// Created by Simon on 2019-04-11.
+//
+
+#ifndef RABBIT2_FRAME_HPP
+#define RABBIT2_FRAME_HPP
+
+#include "geometry.hpp"
+
+namespace Rabbit
+{
+namespace Geometry
+{
+
+template <typename T>
+struct Frame
+{
+public:
+    explicit Frame(const Vector3<T>& n) noexcept
+        : n{ n }
+    {
+        CreateLocalBase(n, s, t);
+    }
+
+    Frame(const Vector3<T>& s, const Vector3<T>& n, const Vector3<T>& t) noexcept
+        : s{ s }, n{ n }, t{ t }
+    {}
+
+    // Transform direction to local space
+    const Vector3<T> ToLocal(const Vector3<T>& dir) const noexcept
+    {
+        return { Dot(dir, s), Dot(dir, n), Dot(dir, t) };
+    }
+
+    // Transform direction to world space
+    const Vector3<T> ToWorld(const Vector3<T>& dir) const noexcept
+    {
+        return dir.x * s + dir.y * n + dir.z * t;
+    }
+
+    // Static utilities to compute values when in local space
+    static constexpr T CosTheta(const Vector3<T>& dir) noexcept
+    {
+        return dir.y;
+    }
+
+    // Local base
+    const Vector3<T> s, n, t;
+};
+
+using Framef = Frame<float>;
+
+} // Geometry namespace
+} // Rabbit namespace
+
+#endif //RABBIT2_FRAME_HPP
